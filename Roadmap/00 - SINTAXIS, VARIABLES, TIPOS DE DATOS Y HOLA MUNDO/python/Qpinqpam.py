059a606c9ecaa7ae032b6dfd171a72ca9032cc0f
Qpinqpam <galerashome@gmail.com>
2025-02-17 12:27:39 +0100

    Create Qpinqpam.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Qpinqpam.py
@@ -0,0 +1,33 @@
+# https://python.org
+
+# comentario en una línea , se abre el comentario con almohadilla
+
+"""
+Esto es un comentario
+en varías líneas
+, se abre con triple comillas
+"""
+'''
+Esto es un comentario
+en varías líneas , pero 
+esta vez , abierto con 
+comillas símples
+'''
+
+my_variable = " mi variable "
+my_variable = " nuevo valor de variable "
+
+
+MY_CONSTANT = " mi constante " # por convención
+MY_CONSTANT = " BOLI VIC "
+
+# datos primitivos de python , son 4
+
+my_int = 1
+my_float = 1,5
+my_bool = true
+my_bool = false
+my_string = " mi cadena de texto "
+my_other_string = ' mi otra cadena de texto '
+
+print ( ¡ Hola , Python ! ) 
